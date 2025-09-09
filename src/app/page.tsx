9f09492d708561547c82c96cb2e9d7d4be0332df
Sobothty <supercrisz24@gmail.com>
2025-09-09 18:43:35 +0700

    add : redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
 "use client";
 
 import { signIn, useSession } from "next-auth/react";
+import { redirect } from "next/navigation";
 
 export default function HomePage() {
-    const { data } = useSession()
-  console.log(data)
-  return <div>Homepage
-    <button onClick={() => signIn("keycloak")}>Click me</button>
-    <div>Acc</div>
-  </div>
+  const { data } = useSession();
+  return redirect(data ? "/dashboard" : "/login");
 }
